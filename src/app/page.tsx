f3ac7aa431ac13698f2a833353218bbde40ef161
ElvizAl <elvizaka77@gmail.com>
2025-05-11 00:01:45 +0700

    hon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { auth } from "@/auth";
 
 
 export default async function Home() {
-  const session = await auth();
   return (
     <div>
-      <p>{session?.user?.email}</p>
-      <p>{session?.user?.name}</p>
+      ,,
     </div>
   );
 }
